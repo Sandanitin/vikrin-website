eec5507ae25d8ff3dfef92a5f87237a9485eb112
VineethK <kyadasuvineeth@gmail.com>
2026-03-04 14:25:23 +0530

    Fix sftp-upload script to include missing backend setup files

--- vikrin-php-backend/sftp-upload.bat
+++ vikrin-php-backend/sftp-upload.bat
@@ -5,4 +5,6 @@ put "C:/Users/sanda/Desktop/vikrin sir/httpsgithub.comvineethkyadasuvikrin-websi
 put "C:/Users/sanda/Desktop/vikrin sir/httpsgithub.comvineethkyadasuvikrin-website.git/vikrin-website/vikrin-php-backend/api/careers-apply.php" public_html/api/careers-apply.php
 put "C:/Users/sanda/Desktop/vikrin sir/httpsgithub.comvineethkyadasuvikrin-website.git/vikrin-website/vikrin-php-backend/api/careers-admin.php" public_html/api/careers-admin.php
 put "C:/Users/sanda/Desktop/vikrin sir/httpsgithub.comvineethkyadasuvikrin-website.git/vikrin-website/vikrin-php-backend/api/admin-login.php" public_html/api/admin-login.php
+put "C:/Users/sanda/Desktop/vikrin sir/httpsgithub.comvineethkyadasuvikrin-website.git/vikrin-website/vikrin-php-backend/api/applications-admin.php" public_html/api/applications-admin.php
+put "C:/Users/sanda/Desktop/vikrin sir/httpsgithub.comvineethkyadasuvikrin-website.git/vikrin-website/vikrin-php-backend/api/setup_applications.php" public_html/api/setup_applications.php
 bye
